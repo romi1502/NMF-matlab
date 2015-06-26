d8ddce4a4a4502b1e9ae7b67648e34f62da56dc0
Romain Hennequin <rhennequin@deezer.com>
2015-06-26 16:00:42 +0200

    Working version of NMFD in python. Cost decreasing not ensured so far

--- python/NMFD.py
+++ python/NMFD.py
@@ -1,104 +1,115 @@
 import numpy as np
+import sys
+from scipy.ndimage.filters import convolve1d
 
-def NMFD(V, R=3 ,T=10 ,n_iter=50, init_W=None, init_H=None):
+def NMFD(V, R=3 ,T=10, n_iter=50, init_W=[], init_H=[]):
     """
-    % NMFD(V, R=3 ,T=10 ,n_iter=50, init_W=None, init_H=None)
-    %    NMFD as proposed by Smaragdis (Non-negative Matrix Factor
-    %    Deconvolution; Extraction of Multiple Sound Sources from Monophonic
-    %    Inputs). KL divergence minimization. The proposed algorithm was
-    %    corrected.
-    %Input :   
-    %   - V : magnitude spectrogram to factorize (is a MxN numpy array)
-    %   - R : number of templates
-    %   - T : template size (in number of frames in the spectrogram)
-    %   - n_iter : number of iterations
-    %   - init_W (optional) : initial value for W.
-    %   - init_H (optional) : initial value for H.
-    %Output :
-    %   - W : time/frequency template (TxMxR array, each template is TxM)
-    %   - H : activities for each template (RxN array)
-    %
-    % Copyright (C) 2015 Romain Hennequin
+     NMFD(V, R=3 ,T=10, n_iter=50, init_W=None, init_H=None)
+        NMFD as proposed by Smaragdis (Non-negative Matrix Factor
+        Deconvolution; Extraction of Multiple Sound Sources from Monophonic
+        Inputs). KL divergence minimization. The proposed algorithm was
+        corrected.
+    Input :
+       - V : magnitude spectrogram to factorize (is a FxN numpy array)
+       - R : number of templates (unused if init_W or init_H is set)
+       - T : template size (in number of frames in the spectrogram) (unused if init_W is set)
+       - n_iter : number of iterations
+       - init_W : initial value for W.
+       - init_H : initial value for H.
+    Output :
+       - W : time/frequency template (FxRxT array, each template is TxF)
+       - H : activities for each template (RxN array)
+
+     Copyright (C) 2015 Romain Hennequin
     """
 
     """
-    % V : spectrogram MxN
-    % H : activation RxN
-    % Wt : spectral template MxR t = 0 to T-1
-    % W : TxMxR
+     V : spectrogram FxN
+     H : activation RxN
+     Wt : spectral template FxR t = 0 to T-1
+     W : FxRxT
     """
     eps = np.spacing(1)
 
     # data size
-    M = V.shape[0];
+    F = V.shape[0];
     N = V.shape[1];
 
     # initialization
-    if init_H:
+    if len(init_H):
         H = init_H
+        R = H.shape[0]
     else:
         H = np.random.rand(R,N);
 
-    if init_W:
+    if len(init_W):
         W = init_W
+        R = W.shape[1]
+        T = W.shape[2]
     else:
-        W = np.random.rand(M,R,T);
+        W = np.random.rand(F,R,T);
 
-    One = np.ones((M,N));
-    Lambda = np.zeros((M,N));
+    One = np.ones((F,N));
+    Lambda = np.zeros((F,N));
 
+    cost = np.zeros(n_iter)
+    for it in range(n_iter):
+        sys.stdout.write('Computing NMFD. iteration : %d/%d' % (it+1, n_iter));
+        sys.stdout.write('\r')
+        sys.stdout.flush()
 
-    for iter in range(n_iter):
-        
         # computation of Lambda
         Lambda[:] = 0;
-        for f in range(M):
+        for f in range(F):
             for z in range(R):
                 cv = np.convolve(W[f,z,:],H[z,:]);
                 Lambda[f,:] = Lambda[f,:] + cv[0:Lambda.shape[1]];
-            
+
         Halt = H.copy();
-        
+
         Htu = np.zeros((T,R,N));
-        Htd = np.zeros((T,R,N)); 
-       
+        Htd = np.zeros((T,R,N));
+
         # update of H for each value of t (which will be averaged)
         VonLambda = V/(Lambda + eps);
-        
-        Hu = np.zeros(R,N);    
-        Hd = np.zeros(R,N);    
+
+        cost[it] = (V*log(V/Lambda)-V+Lambda).sum()
+
+        Hu = np.zeros((R,N));
+        Hd = np.zeros((R,N));
         for z in range(R):
-            for f in range(M):
-                cv = np.convolve(VonLambda[f,:],flipud(W[f,z,:]));
+            for f in range(F):
+                cv = np.convolve(VonLambda[f,:],np.flipud(W[f,z,:]));
                 Hu[z,:] = Hu[z,:] + cv[T-1:T+N-1];
-                cv = np.convolve(One[f,:],flipud(W[f,z,:]));
+                cv = np.convolve(One[f,:],np.flipud(W[f,z,:]));
                 Hd[z,:] = Hd[z,:] + cv[T-1:T+N-1];
-        
-        
+
         # average along t
         H = H*Hu/Hd;
 
         # computation of Lambda
 
         Lambda[:] = 0;
-        for f in range(M):
+        for f in range(F):
             for z in range(R):
-                cv = conv(W[f,z,:],H[z,:])
+                cv = np.convolve(W[f,z,:],H[z,:])
                 Lambda[f,:] += cv[0:Lambda.shape[1]]
 
         mu = 0
         constraint = 1.02**np.arange(0,T)-1
         constraint[0:2] = 0
-        
+
         SumTot = W.sum(axis=1)
-        
+
         VonLambda = V/(Lambda + eps)
-        
-        
+
+
         # update of Wt
         for t in range(T):
-            W[:,:,t] = W[:,:,t] * (  (VonLambda*shiftLR(H,-t+1).T) / (One*shiftLR(H,-t+1).T + eps + mu * constraint[t])  );
-        
-        #print ['computing NMFD. iteration : ' int2str(iter) '/' int2str(n_iter)];
-        
-    return (W, H)
+            shift_H = np.roll(H,-t).T
+            if t>0:
+                shift_H[-t:,:]=0
+
+            W[:,:,t] = W[:,:,t] * (  np.dot(VonLambda,shift_H) / (np.dot(One,shift_H) + eps + mu * constraint[t])  );
+
+    return (W, H, cost)
